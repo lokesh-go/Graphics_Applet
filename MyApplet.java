cbf303207e38e27bf0b7a9cf76088134d675ac93
lokesh525 <lokesh.chandra525@gmail.com>
2018-12-15 15:55:42 +0530

    Developed a Temple using Edit plus editor in java Applet.

---
+++ MyApplet.java
@@ -0,0 +1,159 @@
+import java.applet.*;
+
+import java.awt.Graphics;
+import java.awt.*;
+//<applet code=MyApplet.class width=600 height=600></applet>
+public class MyApplet extends Applet 
+{
+	public void init()
+	{
+	     setBackground(Color.blue);
+	}
+	public void paint(Graphics g)
+	{
+		
+
+     //     Shiv Mandir
+
+		    g.setColor(Color.white);
+			g.drawLine(20,350,170,365);
+			g.drawLine(20,350,60,325);
+			g.drawLine(20,355,60,330);
+			g.drawLine(170,365,350,353);
+			g.drawLine(20,355,170,370);
+			g.drawLine(170,370,350,358);
+			g.drawLine(350,353,300,325);
+			g.drawLine(350,358,300,330);
+
+			g.drawLine(50,460,170,475);
+			g.drawLine(170,475,310,468);
+		
+		
+
+			g.drawLine(170,290,170,365);
+			g.drawLine(300,290,300,355);
+			g.drawLine(60,290,60,350);
+
+
+
+			g.drawLine(165,410,165,475);
+			g.drawLine(165,410,160,410);
+			g.drawLine(160,410,160,400);
+			g.drawLine(160,400,115,400);
+			g.drawLine(175,410,175,475);
+			g.drawLine(175,410,180,410);
+			g.drawLine(180,410,180,400);
+			g.drawLine(180,400,225,400);
+
+			g.drawLine(230,410,230,472);
+			g.drawLine(230,410,225,410);
+			g.drawLine(225,410,225,400);
+			g.drawLine(240,410,240,472);
+			g.drawLine(240,410,245,410);
+			g.drawLine(245,410,245,400);
+			g.drawLine(245,400,285,400);
+			g.drawLine(285,400,285,410);
+			g.drawLine(285,410,290,410);
+			g.drawLine(290,410,290,470);
+
+			g.drawLine(100,410,100,467);
+			g.drawLine(100,410,95,410);
+			g.drawLine(95,410,95,400);
+			g.drawLine(95,400,75,400);
+			g.drawLine(75,400,75,410);
+			g.drawLine(75,410,70,410);
+			g.drawLine(70,410,70,462);
+			g.drawLine(110,410,110,467);
+			g.drawLine(110,410,115,410);
+			g.drawLine(115,410,115,400);
+
+			g.drawLine(300,355,300,470);
+			g.drawLine(60,350,60,460);
+
+
+			g.drawLine(170,50,300,290);
+			g.drawLine(170,50,60,290);
+			g.drawLine(170,35,170,290);
+			g.drawLine(60,290,170,305);
+			g.drawLine(170,305,300,290);
+
+
+
+     //      Parwati Mandir
+
+
+
+
+			g.drawLine(720,450,870,465);
+			g.drawLine(720,450,760,425);
+			g.drawLine(720,455,760,430);
+			g.drawLine(870,465,1050,453);
+			g.drawLine(720,455,870,470);
+			g.drawLine(870,470,1050,458);
+			g.drawLine(1050,453,1000,425);
+			g.drawLine(1050,458,1000,430);
+
+			g.drawLine(750,560,870,575);
+			g.drawLine(870,575,1010,568);
+		
+		
+
+			g.drawLine(870,390,870,465);
+			g.drawLine(1000,390,1000,455);
+			g.drawLine(760,390,760,450);
+
+
+
+			g.drawLine(865,510,865,575);
+			g.drawLine(865,510,860,510);
+			g.drawLine(860,510,860,500);
+			g.drawLine(860,500,815,500);
+			g.drawLine(875,510,875,575);
+			g.drawLine(875,510,880,510);
+			g.drawLine(880,510,880,500);
+			g.drawLine(880,500,925,500);
+
+			g.drawLine(930,510,930,572);
+			g.drawLine(930,510,925,510);
+			g.drawLine(925,510,925,500);
+			g.drawLine(940,510,940,572);
+			g.drawLine(940,510,945,510);
+			g.drawLine(945,510,945,500);
+			g.drawLine(945,500,985,500);
+			g.drawLine(985,500,985,510);
+			g.drawLine(985,510,990,510);
+			g.drawLine(990,510,990,570);
+
+			g.drawLine(800,510,800,567);
+			g.drawLine(800,510,795,510);
+			g.drawLine(795,510,795,500);
+			g.drawLine(795,500,775,500);
+			g.drawLine(775,500,775,510);
+			g.drawLine(775,510,770,510);
+			g.drawLine(770,510,770,562);
+			g.drawLine(810,510,810,567);
+			g.drawLine(810,510,815,510);
+			g.drawLine(815,510,815,500);
+
+			g.drawLine(1000,455,1000,570);
+			g.drawLine(760,450,760,560);
+
+
+			g.drawLine(870,150,1000,390);
+			g.drawLine(870,150,760,390);
+			g.drawLine(870,135,870,390);
+			g.drawLine(760,390,870,405);
+			g.drawLine(870,405,1000,390);
+
+
+
+        //     Link
+
+			g.setColor(Color.red);
+			g.drawLine(170,35,870,135);
+			g.drawLine(170,40,870,140);
+
+		
+	}
+}
+		
